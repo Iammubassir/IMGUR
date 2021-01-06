a85bcecc92101c10ccd9063d5c12e6be11849b8f
Furqhan <furqhan_mubassiruddin@outlook.com>
2021-01-06 18:20:58 +0530

    My Imgur App

---
+++ imgur/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarImgur from './components/navbar';
+
+function App() {
+  return (
+    <div className='App'>
+    <NavbarImgur/>
+    </div>
+  );
+}
+
+
+export default App;
